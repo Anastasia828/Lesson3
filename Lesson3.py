14fe404c62f0eb9b5b1c2302c07936d7b0436c4c
Anastasia828 <aveatik827@gmail.com>
2024-08-18 19:52:24 +0300

    Add files via upload

---
+++ Lesson3.py
@@ -0,0 +1,5 @@
+a_count=12
+b_count=1.5
+first_name='Python'
+c_count=(b_count/a_count)
+print('Курсив:',first_name,',''всего задач:',a_count,','  'затрачено часов:',b_count,',' 'среднее время выполнения:',c_count,'.')
